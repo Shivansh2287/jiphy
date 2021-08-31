ef174572f058353d61ecc26d3127a6d77ddee6e8
Shivansh2287 <54775060+Shivansh2287@users.noreply.github.com>
2021-08-31 23:54:08 +0530

    prod

--- app.js
+++ app.js
@@ -28,15 +28,15 @@ app.use(require("./routes/auth"));
 app.use(require("./routes/post"));
 app.use(require("./routes/user"));
 
-if(process.env.NODE_ENV=="production"){
-  app.use(express.static('frontend/build'))
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static('client/build'))
   const path = require('path')
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
 
-const PORT = process.env.PORT||5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
